71d0c61d1acbaf0b546c5d665435584bd3527824
Adam <adam.gedney@gmail.com>
2014-02-19 18:51:19 -0500

    built http request for getDates api call

--- app/scripts/controllers/dateCtrl.js
+++ app/scripts/controllers/dateCtrl.js
@@ -1,8 +1,36 @@
 'use strict';
 
 angular.module('fullsailsitinApp')
-	.controller('DateCtrl', ['$scope', '$routeParams', function ($scope, $routeParams) {
+	.controller('DateCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
 		console.log($scope, $routeParams.a);
+
+		//GET class dates where acronyms match from API
+		var requestUrl = 'http://127.0.0.1:8887/public/get-dates';
+		var classDay = [];
+		var classDate = [];
+		var classTime = [];
+		$scope.classDates = {};
+
+		$http({method:'GET', url: requestUrl})
+			.success(function(data){
+
+				//on success, loops through class date data, pushing into
+				//an array delivery to ngRepeat.
+				for(var i=0;i<data.length;i++){
+					classDay.push(data[i].day);
+					classDate.push(data[i].date);
+					classTime.push(data[i].time);
+				}
+
+				//sets scope data on success
+				$scope.classDates.classDay = classDay;
+				$scope.classDates.classDate = classDate;
+				$scope.classDates.classTime = classTime;
+
+			})
+			.error(function(data){
+				console.log('get class names error', data);
+			});
 
 
 	}]);
