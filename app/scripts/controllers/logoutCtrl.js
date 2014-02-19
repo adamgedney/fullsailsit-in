9a8f590b24263aa3429506596eb3083bd6334e5b
Adam <adam.gedney@gmail.com>
2014-02-19 16:05:46 -0500

    set remove cookies on logout

--- app/scripts/controllers/logoutCtrl.js
+++ app/scripts/controllers/logoutCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('fullsailsitinApp')
-	.controller('logoutCtrl', ['$scope', '$rootScope', '$window', function ($scope, $rootScope, $window) {
+	.controller('logoutCtrl', ['$scope', '$rootScope', '$window', '$cookies', function ($scope, $rootScope, $window, $cookies) {
 
 		//Called by logout buttons in app header or menu
 		$scope.logoutUser = function(){
@@ -11,7 +11,16 @@ angular.module('fullsailsitinApp')
 			//then spits them back to login page
 			$rootScope.loginObject.$logout();
 			$rootScope.currentUser = '';
+			removeUserCookies();
 			$window.location.href = '#/';
+
+
+			//**set jshint to ignore my function call before declaration
+			function removeUserCookies(){// jshint ignore:line
+				$cookies.name = '';
+				$cookies.avatar = '';
+				$cookies.email = '';
+			}
 		};
 
 	}]);
